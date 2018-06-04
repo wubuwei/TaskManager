4a11b89a22e7f4aa78ccb038c871938a54598518
wubuwei <wubuwei.dev@gmail.com>
2018-06-04 13:39:39 +0000

    v-for,event,custom methods

--- resources/views/tasks/details.blade.php
+++ resources/views/tasks/details.blade.php
@@ -2,8 +2,13 @@
 
 @section('content')
     <div id="app" class="container">
-        <h1>@{{ message }}</h1>
-        <input type="text" v-model="message">
+        <ul class="list-group">
+            <li class="list-group-item" v-for="step in steps">@{{ step.name }}</li>
+            <input type="text" v-model="newStep" @keyup.enter="addStep" class="form-control">
+        </ul>
+        {{--vue2中全局变量$data本身即是json格式，vue1的写法为 @{{ $data | json }}--}}
+        @{{ $data }}
+
     </div>
 @endsection
 
@@ -13,7 +18,16 @@
         new Vue({
             el: '#app',
             data: {
-                message: 'helloworld',
+                steps:[
+                    { name:'fix bugs', completed:false },
+                    { name:'meeting', completed:false },
+                ],
+                newStep:''
+            },
+            methods:{
+                addStep:function () {
+                    this.steps.push({ name:this.newStep, completed:false });
+                }
             }
         });
     </script>
