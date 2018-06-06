b2ac876c6efe8c377f4b1121e4bcd4486b0919b1
wubuwei <wubuwei.dev@gmail.com>
2018-06-06 13:12:29 +0000

    splice,dblclick edit

--- resources/views/tasks/details.blade.php
+++ resources/views/tasks/details.blade.php
@@ -4,22 +4,28 @@
     <div id="app" class="container">
         <h1>未完成的步骤</h1>
         <ul class="list-group">
-            <li class="list-group-item" v-for="step in inProcess">
-                @{{ step.name }}
-                <i class="fa fa-check pull-right" @click="toggleCompletion(step)"></i>
+            <li class="list-group-item" v-for="(step,index) in inProcess">
+                <span @dblclick="editStep(step)">@{{ step.name }}</span>
+                <span class="pull-right">
+                    <i class="fa fa-check" @click="toggleCompletion(step)"></i>
+                    <i class="fa fa-close" @click="removeStep(step)"></i>
+                </span>
             </li>
         </ul>
 
         <form @submit.prevent="addStep" class="form-inline">
-            <input type="text" v-model="newStep" class="form-control">
+            <input type="text" v-model="newStep" ref="newStep" class="form-control">
             <button type="submit" class="btn btn-primary">添加步骤</button>
         </form>
 
         <h1>已完成的步骤</h1>
         <ul class="list-group">
-            <li class="list-group-item" v-for="step in processed">
+            <li class="list-group-item" v-for="(step,index) in processed">
                 @{{ step.name }}
-                <i class="fa fa-check pull-right" @click="toggleCompletion(step)"></i>
+                <span class="pull-right">
+                    <i class="fa fa-check" @click="toggleCompletion(step)"></i>
+                    <i class="fa fa-close" @click="removeStep(step)"></i>
+                </span>
             </li>
         </ul>
 
@@ -45,6 +51,17 @@
                 addStep:function () {
                     this.steps.push({ name:this.newStep, completed:false });
                     this.newStep = '';
+                },
+                removeStep:function(index){
+                    this.steps.splice(index,1);
+                },
+                editStep:function(step){
+                    //移除当前step
+                    this.removeStep(step);
+                    //将当前step加载到input输入框
+                    this.newStep = step.name;
+                    //focus输入框
+                    this.$refs.newStep.focus();
                 },
                 toggleCompletion:function (step) {
                     step.completed = ! step.completed;
