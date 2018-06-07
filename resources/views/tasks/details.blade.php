14550e3a436f597e1eb5766468af6fd6d1096842
wubuwei <wubuwei.dev@gmail.com>
2018-06-07 14:11:18 +0000

    computed,filter(),v-show,v-if

--- resources/views/tasks/details.blade.php
+++ resources/views/tasks/details.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div id="app" class="container">
-        <h1>未完成的步骤</h1>
+        <h2 v-if="remaings.length">未完成的步骤(@{{ remaings.length }})</h2>
         <ul class="list-group">
             <li class="list-group-item" v-for="(step,index) in inProcess">
                 <span @dblclick="editStep(step)">@{{ step.name }}</span>
@@ -13,12 +13,18 @@
             </li>
         </ul>
 
-        <form @submit.prevent="addStep" class="form-inline">
-            <input type="text" v-model="newStep" ref="newStep" class="form-control">
-            <button type="submit" class="btn btn-primary">添加步骤</button>
+        <form @submit.prevent="addStep" class="form-inline,form-horizontal">
+            <div class="form-group col-md-8">
+                <label v-if="! newStep">完成该任务(Task)需要哪些步骤(Step)呢?</label>
+                <input type="text" v-model="newStep" ref="newStep" class="form-control" placeholder="I need to ...">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" v-if="newStep">添加步骤</button>
+            </div>
         </form>
+        <div class="clearfix"></div>
 
-        <h1>已完成的步骤</h1>
+        <h2 v-show="completions.length">已完成的步骤(@{{ completions.length }})</h2>
         <ul class="list-group">
             <li class="list-group-item" v-for="(step,index) in processed">
                 @{{ step.name }}
@@ -30,7 +36,7 @@
         </ul>
 
         {{--vue2中全局变量$data本身即是json格式，vue1的写法为 @{{ $data | json }}--}}
-        @{{ $data }}
+       <pre>@{{ $data }}</pre>
 
     </div>
 @endsection
@@ -76,6 +82,16 @@
                 processed:function () {
                     return this.steps.filter(function (step) {
                         return step.completed;
+                    });
+                },
+                completions:function () {
+                    return this.steps.filter(function (step) {
+                       return step.completed;
+                    });
+                },
+                remaings:function () {
+                    return this.steps.filter(function (step) {
+                        return ! step.completed;
                     });
                 },
             }
