ea2f31c856c932321036c740707824f3dc051313
wubuwei <wubuwei.dev@gmail.com>
2018-06-26 14:40:14 +0000

    vue component deep rebuild step list logic 2

--- resources/assets/js/vue/components/stepList.vue
+++ resources/assets/js/vue/components/stepList.vue
@@ -1,17 +1,20 @@
-
-
 <template>
     <div class="col-md-4 col-md-offset-1">
-        <div class="panel panel-default">
+        <div class="panel panel-default" v-if="show">
             <div class="panel-heading">
-                <h3 v-if="remaings.length">
+                <h3 v-if="remaings.length && type=='remaings'">
                     待完成的步骤({{ remaings.length }})
                     <span class="btn btn-sm btn-info" @click="completeAll">完成所有</span>
+                </h3>
+
+                <h3 v-if="completions.length && type=='completed'">
+                    已完成的步骤({{ completions.length }})
+                    <span class="btn btn-sm btn-danger" @click="clearCompleted">清除所有已完成</span>
                 </h3>
             </div>
             <div class="panel-body">
                 <ul class="list-group">
-                    <li class="list-group-item" v-for="(step,index) in inProcess">
+                    <li class="list-group-item" v-for="(step,index) in typeSwitch">
                         <span @dblclick="editStep(step)">{{ step.name }}</span>
                         <span class="pull-right">
                             <i class="fa fa-check" @click="toggleCompletion(step)"></i>
@@ -22,7 +25,7 @@
             </div>
         </div>
 
-        <div class="panel panel-default">
+        <div class="panel panel-default" v-if="type=='remaings'">
             <div class="panel-heading">
                 <form @submit.prevent="addStep" class="form">
                     <div class="form-group">
@@ -42,7 +45,7 @@
 
 <script>
     export default {
-        props:['steps'],
+        props:['steps','type'],
         data:function(){
             return {
                 newStep:''
@@ -73,12 +76,27 @@
                 //focus输入框
                 this.$refs.newStep.focus();
             },
+            clearCompleted:function () {
+                this.$emit('clear');
+            }
         },
         computed:{
             inProcess:function () {
                 return this.steps.filter(function (step) {
                     return ! step.completed;
                 });
+            },
+            completed:function () {
+                return this.steps.filter(function (step) {
+                    return step.completed;
+                });
+            },
+            typeSwitch:function (){
+                var vm = this;
+                if (vm.type == 'remaings'){
+                    return vm.inProcess;
+                }
+                return vm.completed;
             },
             processed:function () {
                 return this.steps.filter(function (step) {
@@ -94,6 +112,13 @@
                 return this.steps.filter(function (step) {
                     return ! step.completed;
                 });
+            },
+            show:function () {
+                var case1 = this.remaings.length && this.type=='remaings';
+                var case2 = this.completions.length && this.type=='completed';
+                if (case1 || case2){
+                    return true;
+                }
             },
         },
     }
--- resources/views/tasks/details.blade.php
+++ resources/views/tasks/details.blade.php
@@ -8,37 +8,15 @@
     <div id="app" class="container">
         <h1 class="text-muted">{{ $task->title }}</h1>
 
-        <step-list :steps="steps" @toggle="toggleCompletion"
-                   @remove="removeStep" @new="addStep" @complete="completeAll"></step-list>
+        <step-list :steps="steps" @toggle="toggleCompletion" @remove="removeStep"
+                   @new="addStep" @complete="completeAll" type="remaings"></step-list>
 
-        {{--<div class="col-md-4 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 v-show="completions.length">
-                        已完成的步骤(@{{ completions.length }})
-                        <span class="btn btn-sm btn-danger" @click="clearCompleted">清除所有已完成</span>
-                    </h3>
-                </div>
-                <div class="panel-body">
-                    <ul class="list-group">
-                        <step-list  v-for="(step,index) in processed" :step="step"
-                                    @toggle="toggleCompletion(step)" @remove="removeStep(step)"></step-list>
-                    </ul>
-                </div>
-            </div>
-        </div>--}}
-
-        {{--vue2中全局变量$data本身即是json格式，vue1的写法为 @{{ $data | json }}--}}
-       {{--<pre>@{{ $data }}</pre>--}}{{--@{{ $data }}--}}
+        <step-list :steps="steps" @toggle="toggleCompletion" @remove="removeStep"
+                    @clear="clearCompleted" type="completed"></step-list>
 
     </div>
 @endsection
 
 @section('customJS')
     <script src="{{ asset('js/app.js') }}"></script>
-{{--    <script src="{{ asset('js/vue.js') }}"></script>
-    <script src="{{ asset('js/vue-resource.js') }}"></script>
-    <script>
-
-    </script>--}}
 @endsection
